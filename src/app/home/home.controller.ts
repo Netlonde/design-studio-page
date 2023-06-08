90b991c1028d5ffe376381f3636fd93523c3bc5e
Netlonde <carloscomercialti@gmail.com>
2023-06-08 16:27:59 -0300

    feat: config stripe on home page

--- src/app/home/home.controller.ts
+++ src/app/home/home.controller.ts
@@ -1,6 +1,44 @@
+"use client";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import Stripe from "stripe";
+
 function useHomeController() {
-  const nameTest = "Welcome to Home";
-  return { nameTest };
+  const [stripePromise, setStripePromise]: any = useState(null);
+  const [clientSecret, setClientSecret]: any = useState("");
+
+  async function createPaymentIntent() {
+    const stripe = new Stripe(
+      String(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY),
+      {
+        apiVersion: "2022-11-15",
+      }
+    );
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "eur",
+        amount: 1999,
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      setClientSecret(paymentIntent.client_secret);
+    } catch (error: any) {
+      console.log(error);
+      throw new Error("Error");
+    }
+  }
+
+  useEffect(() => {
+    setStripePromise(
+      loadStripe(String(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY))
+    );
+    createPaymentIntent();
+  }, []);
+
+  return { stripePromise, clientSecret };
 }
 
 export default useHomeController;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { PaymentElement, Elements } from "@stripe/react-stripe-js";
+
 import Header from "@/components/header/header";
 import useHomeController from "./home.controller";
 import Background from "./home.style";
@@ -10,26 +14,32 @@ import WhatPeopleSay from "@/components/whatPeopleSay/whatPeopleSay";
 import Questions from "@/components/questions/questions";
 import Footer from "@/components/footer/footer";
 
-export const metadata = {
-  title: "Home",
-  description: "Home page",
-};
-
 export default function Home() {
-  const { nameTest } = useHomeController();
+  const { stripePromise, clientSecret } = useHomeController();
   return (
-    <Background>
-      <Header />
-      <main>
-        <GetStarted />
-        <About />
-        <OurServices />
-        <OurPortfolio />
-        <OurPricing />
-        <WhatPeopleSay />
-        <Questions />
-      </main>
-      <Footer />
-    </Background>
+    <>
+      {clientSecret && stripePromise && (
+        <Background>
+          <Header />
+          <main>
+            <Elements
+              stripe={stripePromise}
+              options={{
+                clientSecret,
+              }}
+            >
+              <GetStarted />
+              <About />
+              <OurServices />
+              <OurPortfolio />
+              <OurPricing />
+              <WhatPeopleSay />
+              <Questions />
+            </Elements>
+          </main>
+          <Footer />
+        </Background>
+      )}
+    </>
   );
 }
